29d9a17a324a489619e3865ec9344df1834e4102
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-06 11:20:51 +0300

    profile model

--- awwards/models.py
+++ awwards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -13,3 +14,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, related_name='profile')
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    profile_photo = models.ImageField(upload_to='images/', blank=True,default='dwf_profile.jpg')
+    user_name = models.CharField(max_length=50, null=True)
+    occupation = models.CharField(max_length=300, null=True)
+    company_name = models.CharField(max_length=300, null=True)
+    bio = models.TextField(blank=True)
+    website = models.CharField(max_length=150, null=True)
+    facebook = models.CharField(max_length=200, null=True)
+    twitter = models.CharField(max_length=200, null=True)
+    instagram = models.CharField(max_length=200, null=True)
+    linkedin = models.CharField(max_length=200, null=True)
