14cdd10e01c5a5f4c5b447cfbf4386d7ba866788
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-06 21:33:34 +0300

    admin

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from .models import Profile,Post,Comment,Location,Followers,Rating
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Location)
+admin.site.register(Followers)
+admin.site.register(Rating)
 
