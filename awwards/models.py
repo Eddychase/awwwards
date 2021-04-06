81be409cf85ce95a5a53aed0e40d9cd91af4a8fd
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-06 11:15:17 +0300

    post model

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    uploaded_by = models.ForeignKey(User, null=True, related_name='posts')
+    country = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=200, null=True)
+
+    @classmethod
+    def all_posts(cls):
+        all_posts = cls.objects.all()
+        return all_posts
+
+    def __str__(self):
+        return self.name
--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def home(request):
+    post = Post.objects.first()
+    post_reviews = post.ratings.all()
+    posts = Post.objects.all()
+
+    print(posts)
