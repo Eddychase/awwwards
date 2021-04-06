18e1d07afaa6c7f92498b45b6103edc43c63f1bb
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-06 16:31:20 +0300

    search function

--- awwards/views.py
+++ awwards/views.py
@@ -32,3 +32,29 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
+def search_results(request):
+    # check if the input field exists and that ic contains data
+    if 'post' in request.GET and request.GET['post']:
+        # get the data from the search input field
+        explore_posts = Post.all_posts()
+        search_term = request.GET.get('post')
+        searched_posts = Post.filter_by_search_term(search_term)
+        print(search_term)
+        caption = f'Search results for {search_term}'
+
+        if len(searched_posts) == 0:
+            caption = f'Results for {search_term} Found'
+        search_context = {
+            'posts': searched_posts,
+            'explore_posts': explore_posts,
+            'caption': caption,
+        }
+        return render(request, 'search.html', search_context)
+    else:
+        explore_posts = Post.all_posts()
+        search_context = {
+            'explore_posts': explore_posts,
+            'caption': 'Matches found for your search!! Discover More Posts'
+        }
+        return render(request, 'search.html', search_context)
